fc1d60a0d03076644f04375cf7ef2fbb1dc9e55e
Kenny <khpham24@gmail.com>
2018-05-16 14:12:53 -0600

    tick done, working on unhardcoding

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -70,23 +70,39 @@ class CPU {
 
     // !!! IMPLEMENT ME
 
-    let IR = this.ram.read(this.reg.PC);
+    let IR = this.ram.read(this.PC);
 
     // Debugging output
-    //console.log(`${this.PC}: ${IR.toString(2)}`);
+    console.log(`${this.PC}: ${IR.toString(2)}`);
 
     // Get the two bytes in memory _after_ the PC in case the instruction
     // needs them.
 
     // !!! IMPLEMENT ME
 
-    let operandA = this.ram.read(this.reg.PC + 1);
-    let operandB = this.ram.read(this.reg.PC + 2);
+    let operandA = this.ram.read(this.PC + 1);
+    let operandB = this.ram.read(this.PC + 2);
 
     // Execute the instruction. Perform the actions for the instruction as
     // outlined in the LS-8 spec.
 
     // !!! IMPLEMENT ME
+    switch (IR) {
+      case 0b10011001: //ldi
+        this.reg[operandA] = operandB;
+        this.pc += 3;
+        break;
+      case 0b01000011: // prn
+        console.log(this.reg[operandA]);
+        this.pc += 2;
+        break;
+      default:
+        console.log(`Unknown instruction at ${this.PC}: ${IR.toString(2)}`);
+        this.stopClock();
+      case 0b00000001: //hlt
+        this.stopClock();
+        break;
+    }
 
     // Increment the PC register to go to the next instruction. Instructions
     // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
@@ -94,7 +110,7 @@ class CPU {
     // for any particular instruction.
 
     // !!! IMPLEMENT ME
-    this.reg.PC += (IR >>> 6) + 1;
+    this.pc += (IR >> 6) + 1;
   }
 }
 
